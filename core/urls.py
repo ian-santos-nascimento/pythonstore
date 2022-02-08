a46b3b55cbcf88dfa0284aa6f19fa752b5d14098
Ian Santos <ian290303@gmail.com>
2022-02-08 19:56:28 -0400

    Tratando exceções de erro 404 e 500)

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-
+from django.conf.urls import handler404, handler500
 from .views import *
 
 urlpatterns = [
     path('', Index, name="index"),
     path('produto/<int:pk>', Produtos, name='produto')
 ]
+
+handler404 = error404
+handler500 = error500
--- core/views.py
+++ core/views.py
@@ -1,4 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.template import loader
+
 from .models import Produto
 
 def Index(request):
@@ -10,8 +14,17 @@ def Index(request):
     return render(request, "index.html", contexto)
 
 def Produtos(request, pk):
-    prod = Produto.objects.get(id=pk)
+    # prod = Produto.objects.get(id=pk)
+    prod = get_object_or_404(Produto, id = pk)
     context = {
         'produto' : prod
     }
     return render(request, 'produto.html', context)
+
+def error404(request,exception):
+    template = loader.get_template('404.html')
+    return HttpResponse(content=template, content_type='text/html; charset=utf8', status=404)
+
+def error500(request):
+    template = loader.get_template('500.html')
+    return HttpResponse(content=template, content_type='text/html; charset=utf8', status=500)
