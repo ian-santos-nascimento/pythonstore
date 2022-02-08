6c1201e8c4e8a8c1435e94d2ae05ec9aa33c29c5
Ian Santos Nascimento <iansantosnascimento@gmail.com>
2022-02-08 17:44:37 -0400

    Testando caminhos de arquivos estáticos do Django

--- python_django_udemy/settings.py
+++ python_django_udemy/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w76xpfd82cz#rwe1wu86ivpg2&2)_))wzwblt#)fx-@pj+-)_^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -119,7 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'  #Durante desenvolvimento
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  #para produção
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
